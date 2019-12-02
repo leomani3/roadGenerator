fbd531ff9a883bc2e0064b52b5a627e87cda659f
Leomani <leoveyssiere.pro@hotmail.fr>
2019-12-02 12:34:29 +0100

    Importation de l'algo de pathfind Commencement de l'adaptation

---
+++ Assets/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int x;
+    public int y;
+    public int cost;
+    public int distance;
+    public Node parent;
+
+    public Node(int posx, int posy, int c, int d)
+    {
+        x = posx;
+        y = posy;
+        cost = c;
+        distance = d;
+        parent = null;
+    }
+
+    public bool Equal(Node n2)
+    {
+        return (this.x == n2.x && this.y == n2.y);
+    }
+}
