d75880db46f61294dfd6142f30db9948b6b4d8f6
Corentin <corentin.chignin@hotmail.fr>
2019-12-02 12:19:28 +0100

    Generation de la map
    perlin noise + obj terrain

---
+++ Assets/Scripts/GenerateMesh.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMesh : MonoBehaviour
+{
+    float[,] map;
+
+    //hauteurs des "montagnes"
+    public int depth;
+    //largeur des "montagnes"
+    public int scale;
+    public int width;
+    public int height;
+
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        map = new float[width, height];  
+        GenerateMap();
+    }
+
+   TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateMap());
+
+        return terrainData;
+    }
+
+    float [,] GenerateMap()
+    {
+        float[,] m = new float[width, height];
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                m[x, y] = CalculPerlinNoise(x, y);
+            }
+        }
+        map = m;
+        return m;
+    }
+
+    float CalculPerlinNoise(int x, int y)
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
