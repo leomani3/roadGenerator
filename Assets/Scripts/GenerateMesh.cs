640f627f53c36242e6ff359d422771f1b7e2784b
Corentin <corentin.chignin@hotmail.fr>
2019-12-09 10:20:53 +0100

    il faut regler cette merde Leo put1

--- Assets/Scripts/GenerateMesh.cs
+++ Assets/Scripts/GenerateMesh.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GenerateMesh : MonoBehaviour
 {
     float[,] map;
+    //Node[,] mapDijkstra;
+    List<Vector2> result;
 
     //hauteurs des "montagnes"
     public int depth;
@@ -12,14 +14,16 @@ public class GenerateMesh : MonoBehaviour
     public int scale;
     public int width;
     public int height;
-
+    public PathFinding dijkstra;
     public GameObject cube;
 
     void Start()
     {
         Terrain terrain = GetComponent<Terrain>();
+        result = new List<Vector2>();
         terrain.terrainData = GenerateTerrain(terrain.terrainData);
-        map = new float[width, height];  
+        map = new float[width, height];
+        //mapDijkstra = new Node[width, height];
         GenerateMap();
         Instantiate(cube, new Vector3(511, map[511, 511]*depth , 511), Quaternion.identity);
         Instantiate(cube, new Vector3(501, map[501, 501] * depth, 501), Quaternion.identity);
@@ -37,15 +41,18 @@ public class GenerateMesh : MonoBehaviour
 
     float [,] GenerateMap()
     {
+        Node[,] mapDijkstra = new Node[width, height];
         float[,] m = new float[width, height];
         for(int x = 0; x < width; x++)
         {
             for (int y = 0; y < width; y++)
             {
                 m[x, y] = CalculPerlinNoise(x, y);
+                mapDijkstra[x, y] = new Node(x, y, 0, 1);
             }
         }
         map = m;
+        result = dijkstra.Dijsktra(mapDijkstra, new Vector3Int(1, 1, 0), new Vector3Int(1, 10, 0));
         return m;
     }
 
